8a3b3659832d250af3d1bc24f6edacec59afe406
Justin Barry <justinbarrythejedi@gmail.com>
2025-09-30 16:23:56 +0700

    evn var chagne

--- config/v_0/setup_env_variables.sh
+++ config/v_0/setup_env_variables.sh
@@ -1,5 +1,5 @@
  export PATH="/workspace/poetry/bin:$PATH"
 export PYENV_ROOT="/workspace/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-export HF_HOME=/workspace/.cache/huggingface
+export HF_HOME="/workspace/.cache/huggingface"
 poetry config virtualenvs.in-project true
